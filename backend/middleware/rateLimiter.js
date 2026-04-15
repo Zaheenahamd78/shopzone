c56248ae948247366df6211609ad94799a57796f
Zaheen Ahmad <zaheenahmad78@example.com>
2026-04-15 12:14:00 +0530

    Fix case: config/redis to Redis in rateLimiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,6 +1,6 @@
 const rateLimit = require('express-rate-limit');
 const RedisStore = require('rate-limit-redis');
-const { redisClient } = require('../config/redis');
+const { redisClient } = require('../config/Redis');
 
 // Make sure redisClient is connected
 if (!redisClient) {
